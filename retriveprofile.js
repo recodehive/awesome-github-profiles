d851468fbdd3ad5d29f4981a53dc24e1c449355b
Nishant Kaushal <101548649+nishant0708@users.noreply.github.com>
2024-08-07 07:28:20 +0530

    fixed

--- retriveprofile.js
+++ retriveprofile.js
@@ -1,6 +1,14 @@
 // Firebase configuration
 var firebaseConfig = {
 //Add Config Files here 
+apiKey: "AIzaSyBEBhBv7AcPhmWS1JwfXijBEarDjsz16xM",
+  authDomain: "lupo-7ba5f.firebaseapp.com",
+  databaseURL: "https://lupo-7ba5f-default-rtdb.firebaseio.com",
+  projectId: "lupo-7ba5f",
+  storageBucket: "lupo-7ba5f.appspot.com",
+  messagingSenderId: "418172032930",
+  appId: "1:418172032930:web:b28842c67139e5c0e6c4fb",
+  measurementId: "G-1NVNFSWR1M"
 };
 
 // Initialize Firebase
