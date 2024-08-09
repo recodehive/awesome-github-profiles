4cda324c69c63da531dd28405d777a32833c6ea4
Anjaliavv51 <154777864+Anjaliavv51@users.noreply.github.com>
2024-08-09 22:44:13 +0530

    Update in script.js code
    #213

--- scripts/revealelementsonscrolljs.js
+++ scripts/revealelementsonscrolljs.js
@@ -7,3 +7,4 @@
         });
         ScrollReveal().reveal('.main-heading ', { delay: 500 , origin:'left' });
         ScrollReveal().reveal('.tag,.search-bar', { delay: 530 , origin:'bottom',interval:200 });
+        ScrollReveal().reveal('.profiles', { delay: 600 , origin:'bottom' });
