bc65def847b9e39af67e96d667d656c7a709016f
Anjaliavv51 <154777864+Anjaliavv51@users.noreply.github.com>
2024-08-09 22:36:21 +0530

    Added effect for all webpages
    #213

--- scripts/revealelementsonscrolljs.js
+++ scripts/revealelementsonscrolljs.js
@@ -1,6 +1,9 @@
-ScrollReveal({ 
-    distance : '60px' ,
-    duration:2500,
-    delay:400
-});
-ScrollReveal().reveal('.profiles .profile-cards,', { delay: 600 , origin:'bottom' });
+
+        ScrollReveal({ 
+            //reset: true ,
+            distance : '60px' ,
+            duration:2500,
+            delay:400
+        });
+        ScrollReveal().reveal('.main-heading ', { delay: 500 , origin:'left' });
+        ScrollReveal().reveal('.tag,.search-bar', { delay: 530 , origin:'bottom',interval:200 });
